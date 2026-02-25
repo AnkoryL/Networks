3f7e049fe6f9d5920df8c81bf8fa5e9210d6b587
Invlasov <invlasov@mail.ru>
2026-02-25 16:05:41 +0300

    Update Networks_install.R

--- data/Networks_install.R
+++ data/Networks_install.R
@@ -1,36 +1,150 @@
 #Rscript "C:/Users/Ankory/Desktop/Networks_install.R"
 #Rscript "C:\ivan_work\Networks\data\Networks_install.R"
-#Rscript "G:\work\Networks_test\Networks_install.R"
-#Rscript "G:\work\my_r_packages\Networks\data\Networks_install.R"
 Sys.setenv(LANG="en")
 user_home <- file.path(Sys.getenv("HOME"))
 user_lib <- file.path(Sys.getenv("HOME"), "R", "libs")
 here<-try(file.create(paste0(user_home,"/","test_test")))
-options(repos = c(CRAN = mirror_list_3[1]))
 if (!file.exists(paste0(user_home,"/","test_test"))) {print("error 1")}
-
-if (!require("BiocManager", quietly = TRUE)) {
-    install.packages("BiocManager",lib=user_lib)
-BiocManager::install(version = "3.22",lib=user_lib)
-
-}
 
 mirror_list_3<-c("https://cloud.r-project.org",
 "https://cran.uni-muenster.de/")
+tries<-15
+# options(repos = c(CRAN = mirror_list_3[1]))
+full_cran_mirror_list<-c("https://cloud.r-project.org/",
+"http://mirror.fcaglp.unlp.edu.ar/CRAN/",
+"https://cran.csiro.au/",
+"https://mirror.aarnet.edu.au/pub/CRAN/",
+"https://cran.ms.unimelb.edu.au/",
+"https://cran.wu.ac.at/",
+"https://www.freestatistics.org/cran/",
+"https://ftp.belnet.be/mirror/CRAN/",
+"https://cran-r.c3sl.ufpr.br/",
+"https://vps.fmvz.usp.br/CRAN/",
+"https://brieger.esalq.usp.br/CRAN/",
+"https://ftp.uni-sofia.bg/CRAN/",
+"https://muug.ca/mirror/cran/",
+"https://mirror.csclub.uwaterloo.ca/CRAN/",
+"https://cran.mirror.rafal.ca/",
+"https://cran.dcc.uchile.cl/",
+"https://mirrors.tuna.tsinghua.edu.cn/CRAN/",
+"https://mirrors.bfsu.edu.cn/CRAN/",
+"https://mirrors.pku.edu.cn/CRAN/",
+"https://mirrors.ustc.edu.cn/CRAN/",
+"https://mirrors.zju.edu.cn/CRAN/",
+"https://mirror-hk.koddos.net/CRAN/",
+"https://mirrors.qlu.edu.cn/CRAN/",
+"https://mirror.lzu.edu.cn/CRAN/",
+"https://mirrors.nju.edu.cn/CRAN/",
+"https://mirrors.sjtug.sjtu.edu.cn/cran/",
+"https://mirrors.sustech.edu.cn/CRAN/",
+"https://mirrors.hust.edu.cn/CRAN/",
+"https://mirrors.nwafu.edu.cn/cran/",
+"https://mirror.uned.ac.cr/cran/",
+"https://mirror.library.ucy.ac.cy/cran/",
+"https://mirrors.dotsrc.org/cran/",
+"https://cran.asia/",
+"https://mirror.cedia.org.ec/CRAN/",
+"https://cran.030-datenrettung.de/",
+"https://pbil.univ-lyon1.fr/CRAN/",
+"https://mirror.ibcp.fr/pub/CRAN/",
+"https://cran.asnr.fr/",
+"https://ftp.fau.de/cran/",
+"https://cran.datenrettung360.de/",
+"https://ftp.gwdg.de/pub/misc/cran/",
+"https://mirror.dogado.de/cran/",
+"https://cran.uni-muenster.de/",
+"https://mirror.clientvps.com/CRAN/",
+"https://mirror.kamp.de/cran/",
+"https://ftp.cc.uoc.gr/mirrors/CRAN/",
+"https://cran.r-project.hu/",
+"https://cran.hafro.is/",
+"https://cran.icts.res.in/",
+"https://mirror.niser.ac.in/cran/",
+"https://cran.isid.ac.in/",
+"https://cran.usk.ac.id/",
+"https://cran.um.ac.ir/",
+"https://cran.mirror.garr.it/CRAN/",
+"https://cran.stat.unipd.it/",
+"https://ftp.yz.yamagata-u.ac.jp/pub/cran/",
+"https://cran.itam.mx/",
+"https://est.colpos.mx/",
+"https://mirror.marwan.ma/cran/",
+"https://mirrors.evoluso.com/CRAN/",
+"https://mirror.lyrahosting.com/CRAN/",
+"https://cran.stat.auckland.ac.nz/",
+"https://cran.uib.no/",
+"https://cran.radicaldevelop.com/",
+"https://mirror.truenetwork.ru/CRAN/",
+"https://mirror.maeen.sa/cran/",
+"https://ftp.cixug.es/CRAN/",
+"https://cran.rediris.es/",
+"https://mirror.accum.se/mirror/CRAN/",
+"https://stat.ethz.ch/CRAN/",
+"https://mirror.metanet.ch/cran/",
+"https://cran.csie.ntu.edu.tw/",
+"https://www.stats.bris.ac.uk/R/",
+"https://cran.ma.imperial.ac.uk/",
+"https://mirror.las.iastate.edu/CRAN/",
+"http://ftp.ussg.iu.edu/CRAN/",
+"https://mirror.its.umich.edu/cran/",
+"https://cran.wustl.edu/",
+"https://archive.linux.duke.edu/cran/",
+"https://cran.case.edu/",
+"https://ftp.osuosl.org/pub/cran/",
+"https://lib.stat.cmu.edu/R/CRAN/",
+"https://cran.mirrors.hoobly.com/",
+"https://mirrors.nics.utk.edu/cran/",
+"https://mirror.chpc.utah.edu/pub/cran/",
+"https://cran.nyuad.nyu.edu/",
+"https://espejito.fder.edu.uy/cran/",
+"https://mirrors.cicku.me/cran/")
+
+mirror_selector<-function(iteration,mirror_list) {
+mirror_list_pos<-(iteration %% length(iteration))+1
+current_mirror<-mirror_list[mirror_list_pos]
+return(current_mirror)
+}
+
+mirror_list_cran=sample(full_cran_mirror_list,size=length(full_cran_mirror_list))
+mirror_list_cran<-c("https://cloud.r-project.org",mirror_list_cran)
+
+for (i in 1:tries) {
+if (require("BiocManager", quietly = TRUE,character.only=TRUE,lib=lib_path)) {stop()}
+tryCatch({
+if (!require("BiocManager", quietly = TRUE,character.only=TRUE,lib=lib_path)) {
+	current_mirror<-mirror_selector(i,mirror_list_cran)
+	install.packages("BiocManager",lib=user_lib,repos=current_mirror)
+	options(repos=NULL)
+	chooseBioCmirror(ind=as.character(1))
+	BiocManager::install(version = "3.22",lib=user_lib)
+}
+}, error=function(cond){
+					message(paste0("Error while trying to install package ", "BiocManager"))
+					message("Original error message:")
+					message(conditionMessage(cond))
+				}, warning=function(cond) {
+					message(paste0("Install packages caused a warning while installing  ", "BiocManager"))
+					message("Original warning message:")
+					message(conditionMessage(cond))
+				}, finally={
+						is_pkg_i_installed_already<-suppressMessages(require("BiocManager",character.only=TRUE,lib=user_lib))
+						successfull_install<-is_pkg_i_installed_already
+						if (successfull_install) {outcome<-"successfully"} else {outcome<-"unsuccessfully"}
+						message(paste0("Package ", "BiocManager", " was installed ", outcome," at try ",i))
+				})
+}
+is_installed<-suppressMessages(require("BiocManager",character.only=TRUE,lib=user_lib))
 
 
 
-
-
-
-persistent_install_packages<-function(pkgs,...,randomize_mirror_order=FALSE,global_tries_max=2,force_reinstall=FALSE,mirror_list_cran=NA,mirror_list_bioc=NA,mode_cran_or_bioc=c("CRAN"),lib_path=file.path(Sys.getenv("HOME"), "R", "libs")) {
+persistent_install_packages<-function(pkgs,...,randomize_mirror_order=FALSE,global_tries_max=2,force_reinstall=FALSE,mirror_list_cran=NA,mirror_list_bioc=1,mode_cran_or_bioc=c("CRAN"),lib_path=file.path(Sys.getenv("HOME"), "R", "libs")) {
 	#The idea of this fucntion is to be as hands off and persistent as reasonable in installing the package in the enviornment of not-so-stable internet connection.
 	#Well, if that is the idea - we should have an individual access to every package installation
 	#Therefore, we are feeding install packages or bioc manager with packages 1 by 1.
 	acceptable_modes<-tolower(as.character(c("CRAN","Bioconductor",1,2)))
 	mode_cran_or_bioc<-tolower(as.character(mode_cran_or_bioc[1]))
-	if (is.na(mirror_list_bioc)) {mirror_list_bioc<-as.character(1:16)}
-	if (is.na(mirror_list_cran)) {mirror_list_cran<-c("https://cloud.r-project.org",
+	if (is.na(mirror_list_bioc[1])) {mirror_list_bioc<-as.character(1:16)}
+	if (is.na(mirror_list_cran[1])) {mirror_list_cran<-c("https://cloud.r-project.org",
 	"https://cran.uni-muenster.de/")}
 
 	if (!(mode_cran_or_bioc %in% acceptable_modes)) {
@@ -59,12 +173,18 @@ persistent_install_packages<-function(pkgs,...,randomize_mirror_order=FALSE,glob
 		}
 			while ((global_tries<global_tries_max) & !(successfull_install)) {
 				global_tries<-global_tries+1
-				mirror_list_pos<-(global_tries %% length(mirror_list))+1 
+				if (mode_cran_or_bioc==tolower("CRAN")) {
+					current_mirror<-mirror_selector(global_tries,mirror_list_cran)
+					
+				} else if ((mode_cran_or_bioc==tolower("Bioconductor")) ){
+					current_mirror<-mirror_selector(global_tries,mirror_list_cran)
+				} else {
+				stop(paste0("Error mode ", mode_cran_or_bioc))
+				}
 				tryCatch({
 				
 				if (mode_cran_or_bioc==tolower("CRAN")) {
 						message<-paste0("Runnin install packages to install ", pkg_i, " in mode ",mode_cran_or_bioc, " from mirror ", current_mirror)
-						current_mirror<-mirror_list_cran[mirror_list_pos]
 						install.packages(pkg_i,dependencies=TRUE,lib=lib_path,repos=current_mirror,force = FALSE)
 						message<-paste0("Install packages ran sucesfully")
 						print(message)
@@ -73,7 +193,7 @@ persistent_install_packages<-function(pkgs,...,randomize_mirror_order=FALSE,glob
 					# print("test")
 						message<-paste0("Runnin BIOCMANAGER INSTALL to install ", pkg_i, " in mode ",mode_cran_or_bioc, " from mirror ", current_mirror)
 						print(message)
-						current_mirror<-mirror_list_bioc[mirror_list_pos]
+						options(repos=NULL)
 						chooseBioCmirror(ind=as.character(current_mirror))
 						BiocManager::install(pkgs=pkg_i,dependencies=TRUE,lib=lib_path,force = FALSE)
 						message<-paste0("BIOCmanager finished sucesfully")
@@ -83,7 +203,7 @@ persistent_install_packages<-function(pkgs,...,randomize_mirror_order=FALSE,glob
 
 
 			
-					}
+					
 				}, error=function(cond){
 					message(paste0("Error while trying to install package ", pkg_i, " using mirror ",current_mirror))
 					message("Original error message:")
@@ -101,8 +221,8 @@ persistent_install_packages<-function(pkgs,...,randomize_mirror_order=FALSE,glob
 			
 			}
 	}
-}
 
+}
 
 rprofile_path <- file.path(user_home, ".Rprofile")
 if (!file.exists(rprofile_path)) {
@@ -122,8 +242,13 @@ if (!dir.exists(user_lib)) dir.create(user_lib, recursive = TRUE, showWarnings =
 
 # install.packages("BiocManager",lib=user_lib)
 # persistent_install_packages(c("BiocManager","remote","dplyr"),lib=user_lib,randomize_mirror_order=TRUE,global_tries_max=10,force_reinstall=TRUE,mirror_list=mirror_list_3)
-persistent_install_packages(c("dplyr"),mode_cran_or_bioc=1,lib=user_lib,randomize_mirror_order=TRUE,global_tries_max=10,force_reinstall=TRUE,mirror_list=mirror_list_3)
-persistent_install_packages(c("AnnotationDbi"),mode_cran_or_bioc=2,lib_path=user_lib,randomize_mirror_order=FALSE,global_tries_max=10,force_reinstall=TRUE,mirror_list=1:3)
+persistent_install_packages(c("remote","dplyr"),mode_cran_or_bioc=1,lib=user_lib,randomize_mirror_order=TRUE,global_tries_max=tries,force_reinstall=FALSE,mirror_list_cran=full_cran_mirror_list)
+persistent_install_packages(c("AnnotationDbi",
+  "org.Hs.eg.db",
+  "org.Mm.eg.db",
+  "org.Mmu.eg.db",
+  "org.Dr.eg.db"
+),mode_cran_or_bioc=2,lib_path=user_lib,randomize_mirror_order=FALSE,global_tries_max=tries,force_reinstall=FALSE,mirror_list_bioc=1:16)
 
 # BiocManager::install(c(
   # "AnnotationDbi",
@@ -133,23 +258,21 @@ persistent_install_packages(c("AnnotationDbi"),mode_cran_or_bioc=2,lib_path=user
   # "org.Dr.eg.db"
 # ), lib = user_lib, ask = FALSE, force = TRUE)
 
-#zip_url <- "https://github.com/AnkoryL/Networks/archive/refs/heads/main.zip"
-#zip_dest <- file.path(user_home, "Networks-main.zip")
-#download.file(zip_url, zip_dest, mode = "wb")
-#unzip(zip_dest, exdir = user_home)
-#pkg_dir <- file.path(user_home, "Networks-main")
-#remotes::install_local(pkg_dir, lib = user_lib, force = TRUE, dependencies = TRUE)
+zip_url <- "https://github.com/AnkoryL/Networks/archive/refs/heads/main.zip"
+zip_dest <- file.path(user_home, "Networks-main.zip")
+download.file(zip_url, zip_dest, mode = "wb")
+unzip(zip_dest, exdir = user_home)
+pkg_dir <- file.path(user_home, "Networks-main")
+remotes::install_local(pkg_dir, lib = user_lib, force = TRUE, dependencies = TRUE)
 
-# remotes::install_github("AnkoryL/Networks",
-			# ref = "main",
-			# lib = user_lib,
-			# force = TRUE,
-			# dependencies = c("Imports", "LinkingTo"),
-			# build_vignettes = FALSE,
-			# upgrade = "never")
-test_f<-function () {
-		options(repos = c(CRAN = mirror_list_3[1]))
-		pkg_i2<-"KEGGREST"
-		BiocManager::install(pkg_i2,dependencies=FALSE,lib=user_lib,force = TRUE)
-}
-test_f()
+remotes::install_github("AnkoryL/Networks",
+			ref = "main",
+			lib = user_lib,
+			force = TRUE,
+			dependencies = c("Imports", "LinkingTo"),
+			build_vignettes = FALSE,
+			upgrade = "never")
+			
+is_installed<-suppressMessages(require("Networks",character.only=TRUE,lib=user_libs))
+if (is_installed) {outcome<-"successfully"} else {outcome<-"unsuccessfully"}
+paste0("Package ","Networks", " was installed ", outcome)
